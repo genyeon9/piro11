55cd41d3b721dbc20c1b89d7f621840ee07b77bd
jinhyeon <kjh_5771@naver.com>
2019-07-19 22:06:15 +0900

    blog.admin.PostAdmin에 list_display 커스텀

--- 3주차_금요일_과제/practice1/blog/admin.py
+++ 3주차_금요일_과제/practice1/blog/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from .models import Post
 
 # Register your models here.
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'content_size', 'created_at', 'updated_at']
+
+    def content_size(self, post):
+        return mark_safe('{}'.format(len(post.content)))
+    content_size.short_description = '글자수'
+
+#admin.site.register(Post, PostAdmin)
