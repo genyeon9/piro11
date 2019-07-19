7c85ed0c15b1f2a6936fc21389c8c59ad6d680ed
jinhyeon <kjh_5771@naver.com>
2019-07-19 17:12:47 +0900

    blog.models.lnglat_validator 함수생성/유효셩검증

--- 3주차_금요일_과제/practice1/blog/models.py
+++ 3주차_금요일_과제/practice1/blog/models.py
@@ -1,10 +1,20 @@
+import re
 from django.db import models
+from django.forms import ValidationError
 
 # Create your models here.
+
+def lnglat_validator(value):
+    if not re.match(r'^([+-]?\d+\.?\d*),([+-]?\d+\.?\d*)$', value):
+        raise ValidationError('Invalid LngLat Type')
 
 class Post(models.Model):
     title = models.CharField(max_length=100, verbose_name='제목',
         help_text='포스팅 제목을 입력해주세요. 최대 100자 내외.') #길이 제한 있는 문자열
     content = models.TextField(verbose_name='내용') #길이 제한 없는 문자열
+    tags = models.CharField(max_length=100, blank=True)
+    lnglat = models.CharField(max_length=50, blank=True, help_text='경도/위도 포맷으로 입력',
+        validators=[lnglat_validator],
+        )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
