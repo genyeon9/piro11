aaa4f448bb351d6ff187826bf52f677e41384c99
jinhyeon <kjh_5771@naver.com>
2019-07-21 14:23:20 +0900

    blog/templates/blog/layout.html 부모 템플릿을 정의하고 상속 처리

--- 3주차_금요일_과제/practice1/shop/models.py
+++ 3주차_금요일_과제/practice1/shop/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Post(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='+')
         #related_name='+'는 related_name을 쓰지 않겠다는 뜻
+        #related_name 중복을 피하려면 각각의 related_name을 지정해준다
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
