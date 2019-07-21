de8dc489424a41460d1fcb2d9ae1d56c24556e6c
jinhyeon <kjh_5771@naver.com>
2019-07-21 13:57:45 +0900

    related_name 예시로서, shop.models.Post 모델 생성

--- 3주차_금요일_과제/practice1/shop/models.py
+++ 3주차_금요일_과제/practice1/shop/models.py
@@ -1,3 +1,11 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='+')
+        #related_name='+'는 related_name을 쓰지 않겠다는 뜻
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
